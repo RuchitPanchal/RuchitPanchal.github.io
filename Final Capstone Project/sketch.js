de160bd02319b63c85b8a196ce33ba464e381f1a
RuchitPanchal <ruchitpanchal05@gmail.com>
2023-12-14 11:14:10 -0600

    made the chess board

--- Final Capstone Project/sketch.js
+++ Final Capstone Project/sketch.js
@@ -5,11 +5,43 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+// Global Variables 
+let squareSize = 100;
+let numRows = 8;
+let numCols = 8;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
 }
+
+
 
 function draw() {
   background(220);
+  drawChessBoard();
 }
+
+
+function drawChessBoard() {
+  // Nested  Loop 
+  for (let i = 0; i < numCols; i++) {
+    for (let c = 0; c < numRows; c++) {
+      if ((i + c )% 2 === 0) {
+        fill(255);// WHite Squares 
+
+      } else {
+        fill(0); //Black Squares
+
+      }
+
+
+      rect(i * squareSize, c * squareSize, squareSize, squareSize);
+    }
+
+
+  }
+}
+
+
+
