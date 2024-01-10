8608ae7a29a7ba3548756f3195cecc8e4a80a682
RuchitPanchal <ruchitpanchal05@gmail.com>
2024-01-10 11:34:57 -0600

    Update sketch.js

--- Final Capstone Project/sketch.js
+++ Final Capstone Project/sketch.js
@@ -6,77 +6,109 @@
 // - describe what you did to take this project "above and beyond"
 
 // Global Variables 
-class tile {
-  constructor(i, j, piece = null) {
-    this.i = i
-    this.j = j
-    this.piece = piece
-    this.selected = false
-    this.col = null
-  }
+let squareSize = 100;
+let numRows = 8;
+let numCols = 8;
+let board = [];
 
-  show() {
-    let {
-      i,
-      j
-    } = this
-    strokeWeight(1);
-    noStroke();
-    if (!this.col) {
-      fill("#DCD9CD")
-      if (i % 2 == 1) {
-        if (j % 2 == 0)
-          fill("#70552f")
-      } else if (i % 2 != 1) {
-        if (j % 2 != 0)
-          fill("#70552f")
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+}
+
+
+
+function draw() {
+  background(220);
+  drawChessBoard();
+}
+
+
+function drawChessBoard() {
+  // Nested  Loop 
+  for (let i = 0; i < numCols; i++) {
+    for (let c = 0; c < numRows; c++) {
+      if ((i + c) % 2 === 0) {
+        fill(255);// WHite Squares 
+
+      } else {
+        fill(0); //Black Squares
+
       }
-    } else {
-      fill(this.col);
+
+
+      rect(i * squareSize, c * squareSize, squareSize, squareSize);
     }
 
-    rect(this.x, this.y, scl, scl);
 
-    if (this.selected) {
-      let col = color(101)
-      if (this.i % 2 == 1) {
-        if (this.j % 2 == 0)
-          col = color(51)
-      } else if (this.i % 2 != 1) {
-        if (this.j % 2 != 0)
-          col = color(51)
-      }
-      fill(col)
-      rect(this.x, this.y, scl, scl);
-
-      if (!this.col)
-        this.piece.showMoves();
+  }
+}
+function makepieces() {
+  for (let i = 0; i < 8; i++) {
+    for(let j = 0 ; j< 8; j++){
+     let piece = board[i][j];
+     if (piece !== null) {
+      piece.show(i,j);
+     }
     }
-    if (this.piece) {
-      image(this.piece.image, this.x, this.y);
-    }
-    this.col = null
-  }
 
-  get x() {
-    return this.i * scl
-  }
-
-  get y() {
-    return this.j * scl
-  }
-
-  contains(p) {
-    let {
-      x,
-      y
-    } = this
-    return Findpoint(x, y, x + scl, y + scl, p.x, p.y)
   }
 }
 
-function Findpoint(x1, y1, x2, y2, x, y) {
-  return (x > x1 && x < x2 && y > y1 && y < y2)
+
+
+function mouseClicked(){
+  let x = Math.floor(mouseX , width/8);
+  let y = Math.floor(mouseY , height /8);
+
+  let selectedPiece = board[x][y];
+  
+
+
+
 }
 
+
+class Piece {
+  constructor(x, y, color) {
+    this.x = x;
+    this.y = y;
+    this.color = color;
+
+    show(){
+      textSize(30);
+      fill(this.color === "white" ? 255 : 0);
+      text("P", x * (width/8) + 0.35 * (width /8), y * (height/8) + 0.7 * (height/8));
+    move(newX , newY){
+      // Adding later 
+
+    }
+    class Rook extends Piece{
+      constructor(x , y, color){
+        super(x, y, color);
+      }
+      show(){
+        textSize(30);
+        fill(this.color === "white" ? 255: 0);
+        text("R" , x *(width/8 )+ 0.35 *(width/8), y*(height/8) + 0.7 * (height/8) );
+        move (newX, newY){
+          // Later
+        }
+
+      }
+
+      
+        
+      }
+
+        
+      
+
+    }
+
+    }
+
+  }
 
